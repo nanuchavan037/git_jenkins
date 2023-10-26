da63004a8ecb831faf380639ee3fe41a05f407d1
nanuchavan037 <77206807+nanuchavan037@users.noreply.github.com>
2023-10-26 13:59:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import psutil
+import time
+
+def print_hello():
+    print("Hello, World!")
+
+def monitor_system_resources(interval_seconds=10):
+    while True:
+        cpu_percent = psutil.cpu_percent()
+        mem = psutil.virtual_memory()
+        print(f"CPU Usage: {cpu_percent}%")
+        print(f"Memory Usage: Total={mem.total} MB, Available={mem.available} MB, Used={mem.used} MB")
+        print('-' * 40)
+        time.sleep(interval_seconds)
+
+if __name__ == "__main__":
+    print_hello()
+    monitor_system_resources()
